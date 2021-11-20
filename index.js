5af6e8673d1f9bacb817fc7ddbd8b06d82487002
Pritam Barua <pritambarua@Pritams-MacBook-Air.local>
2021-11-20 15:46:13 +0530

    code changes

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+const fs = require('fs');
 
-const main = () => {
+const main = (path) => {
+    return new Promise((resolve, reject) => {
+    if(fs.lstatSync(path).isDirectory()){
+        reject(new Error('Path is a Directory'));
+    }
 
+      fs.readFile(path, (err, data) =>{
+            if(!err)
+            {
+                try{
+                    let js = JSON.parse(data);
+                    resolve(js);
+                } catch(e){
+                    reject(new Error('JSON Invalid'))
+                }
+            } else {
+                reject(new Error('File Does not exist'));
+            }
+        });
+});
 };
 
 
